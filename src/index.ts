efc34fc4b9a8fe4f118b56c47a9b8d10778a6d2e
Roger Chappel <miscanalysis@gmail.com>
2026-05-05 06:35:32 +1000

    feat: expose library API

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { SafetydeckError } from './errors.js';
+export { loadProfile, parseProfile } from './profile.js';
+export { loadTemplate, parseTemplate } from './template.js';
+export { generatePlan } from './planner.js';
+export { renderMarkdown } from './markdown.js';
+export type { ChecklistItem, ChecklistTemplate, GenerateOptions, PlanItem, SafetyPlan, SafetyProfile } from './types.js';
